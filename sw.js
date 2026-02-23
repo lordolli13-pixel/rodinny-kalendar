696bc93513179117e707c7fb395068b70d7a1a68
lordolli13-pixel <lord.olli13@gmail.com>
2026-02-23 09:38:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
-const CACHE_NAME = 'family-cal-slate-v4';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'family-cal-v4';
+const ASSETS = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });

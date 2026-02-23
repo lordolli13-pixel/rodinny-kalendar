9b8a104b39626c7c4e0a8eb766f0a8798da0d86d
lordolli13-pixel <lord.olli13@gmail.com>
2026-02-23 16:55:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,41 @@
-const CACHE_NAME = 'family-cal-v4';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'fam-cal-v6-cache';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
+// Instalace - uložení souborů pro offline běh
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// Aktivace - vyčištění staré mezipaměti
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
+});
+
+// Fetch - obsluha požadavků i bez sítě
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+// Poslech na kliknutí na notifikaci
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) return clientList[0].focus();
+      return clients.openWindow('./');
+    })
+  );
 });
